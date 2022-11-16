ac9e30fb968e1238d1e6a17c69d2bdc9fd737178
JR <41521299+taquero-programador@users.noreply.github.com>
2022-11-16 02:27:09 -0600

    Delete hello-rust/target directory

--- hello-rust/target/debug/deps/hello_rust-ff3bfb61569a5d95.d
+++
@@ -1,5 +0,0 @@
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/deps/hello_rust-ff3bfb61569a5d95: src/main.rs
-
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/deps/hello_rust-ff3bfb61569a5d95.d: src/main.rs
-
-src/main.rs:
