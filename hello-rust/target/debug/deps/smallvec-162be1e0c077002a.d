92e2c296b825df68f7eabc008e27798c29e1367f
JR <41521299+taquero-programador@users.noreply.github.com>
2022-11-16 02:31:20 -0600

    Delete hello-rust/target directory

--- hello-rust/target/debug/deps/smallvec-162be1e0c077002a.d
+++
@@ -1,7 +0,0 @@
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/deps/smallvec-162be1e0c077002a.rmeta: /home/karen/.cargo/registry/src/github.com-1285ae84e5963aae/smallvec-1.10.0/src/lib.rs
-
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/deps/libsmallvec-162be1e0c077002a.rlib: /home/karen/.cargo/registry/src/github.com-1285ae84e5963aae/smallvec-1.10.0/src/lib.rs
-
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/deps/smallvec-162be1e0c077002a.d: /home/karen/.cargo/registry/src/github.com-1285ae84e5963aae/smallvec-1.10.0/src/lib.rs
-
-/home/karen/.cargo/registry/src/github.com-1285ae84e5963aae/smallvec-1.10.0/src/lib.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/karen/Documentos/javier/git/rust/hello-rust/target/debug/hello-rust: /home/karen/Documentos/javier/git/rust/hello-rust/src/main.rs
